8d0969434d5c8e0e6b72d3727e74f696ab4b3974
Garet Webster <gdubrocks@gmail.com>
2016-02-18 00:22:42 -0800

    Initial commit

--- client/app/scripts/controllers/dashboard.js
+++ client/app/scripts/controllers/dashboard.js
@@ -10,6 +10,33 @@
 angular.module('iReceptionistApp')
 .controller('DashboardCtrl', function($rootScope, AppointmentService) {
     $rootScope.toolbarTitle = 'Dashboard';
-
+	
+	$rootScope.todos = [
+      {
+        number: '(123) 456-7890',
+        visitor: 'Giacomo Guilizzoni',
+        employee: 'Amanda',
+        notes: "Just a check up"
+      },
+      {
+        number: '(123) 456-7890',
+        visitor: 'Marco Botton',
+        employee: 'Peter',
+        notes: ""
+      },
+      {
+        number: '(123) 456-7890',
+        visitor: 'Mariah Maclachlan',
+        employee: 'Venkman',
+        notes: ""
+      },
+      {
+        number: '(123) 456-7890',
+        visitor: 'Valerie Liberty',
+        employee: 'Powell',
+        notes: ""
+      },
+    ];
+	
     console.log('DashboardCtrl loaded.');
 });
