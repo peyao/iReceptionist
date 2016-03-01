c689620f39f99fbf700375166c3f1bcdd70f677b
Amanda Luff <aluff@ucsd.edu>
2016-03-01 02:49:49 -0800

    Removed deprecated AppointmentService dependency from Settings Controllers

--- client/app/scripts/controllers/settings_account.js
+++ client/app/scripts/controllers/settings_account.js
@@ -6,7 +6,7 @@
  * Controller for the settings page
  */
 angular.module('iReceptionistApp')
-    .controller('SettingsAccountCtrl', function($rootScope, $scope, AppointmentService) {
+    .controller('SettingsAccountCtrl', function($rootScope, $scope) {
         $rootScope.currentState = 'settings-account';
         $('#page-content-ui-view').resize(function() {
             $('#page-content-ui-view').width($rootScope.pageContentWidth());
