180b834bc694f0bfc3b3d439a15b216dda4919d1
Erik Xu <dixu@ucsd.edu>
2016-03-02 16:46:03 -0800

    - Automatically log-in after registration of new account - Need to work on checking for unique email and functionalities of registration steps

--- client/auth/scripts/controllers/register.js
+++ client/auth/scripts/controllers/register.js
@@ -6,7 +6,7 @@
  * Controller for the registration page
  */
 angular.module('iReceptionistApp')
-.controller('RegisterCtrl', function($rootScope, $scope, $http, $window, AuthenticationService, DropZone) {
+.controller('RegisterCtrl', function($rootScope, $scope, $http, $window, $cookies, AuthenticationService, DropZone) {
 
     var REGISTRATION_STEPS = 4;
     $scope.step = 1;
@@ -54,16 +54,58 @@ angular.module('iReceptionistApp')
             // Success
             function (regObj) {
                 console.log('register success');
-                var path = '/app'
-                $window.location.href = path; // Redirect
+
+                //
+                // Force re-direct back to log-in screen
+                //
+                //var path = '/app'
+                //$window.location.href = path; // Redirect
+
+
+                //
+                // Automatically log-in after registration
+                //
+                AuthenticationService.login(
+                    {
+                        'email': $scope.register.step1.email,
+                        'password': $scope.register.step1.password
+                    },
+
+                    // Success
+                    function(userObj) {
+                        // Need to set path because we are going from '/auth' to '/app' or '/vip'
+                        // TODO: On VIP side, need to use token to reverify the user has the correct role
+                        // or else log them off because they don't belong there.
+                        // TODO: For now, just do local role level check here and redirect.
+
+                        var path = '/app';
+                        if (userObj.user.role === -1) {
+                            path = '/vip';
+                        }
+                        $cookies.put('user', userObj.user, {'path': '/auth'});
+                        $cookies.put('token', userObj.token, {'path': '/auth'});
+                        $cookies.put('user', userObj.user, {'path': path});
+                        $cookies.put('token', userObj.token, {'path': path});
+                        $window.location.href = path; // Redirect
+                    },
+                    // Failure
+                    function(err) {
+                        //$scope.alert.danger = err.errorMsg;
+                        console.log('log in fail');
+                    }
+                );
             },
 
             // Error
             function (err) {
                 console.log('register fail');
                 //$scope.alert.danger = err.errorMsg;
-                var path = '/app'
-                $window.location.href = path; // Redirect
+
+                //
+                // Force re-direct back to log-in screen
+                //
+                //var path = '/app'
+                //$window.location.href = path; // Redirect
             }
         );
 
