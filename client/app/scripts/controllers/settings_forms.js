59146ac9f0c7882b21caf538419c532dc544ab6a
Samantha Hahn <smhahn@ucsd.edu>
2016-03-06 12:03:40 -0800

    Rename forms and themes page to be forms

--- client/app/scripts/controllers/settings_forms.js
+++ client/app/scripts/controllers/settings_forms.js
@@ -2,13 +2,13 @@
  * @ngdoc function
  * @name iReceptionistApp.controller:SettingsCtrl
  * @description
- * # SettingsFormsThemesCtrl
+ * # SettingsFormsCtrl
  * Controller for the settings page
  */
 angular.module('iReceptionistApp')
-    .controller('SettingsFormsThemesCtrl', function($scope, $builder, $validator, $rootScope) {
-        $rootScope.currentState = 'settings-forms-themes';
-        console.log('SettingsFormsThemesCtrl loaded.');
+    .controller('SettingsFormsCtrl', function($scope, $builder, $validator, $rootScope) {
+        $rootScope.currentState = 'settings-forms';
+        console.log('SettingsFormsCtrl loaded.');
 
         $('#page-content-ui-view').resize(function() {
             $('#page-content-ui-view').width($rootScope.pageContentWidth());
