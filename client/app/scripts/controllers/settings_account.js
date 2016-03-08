3334a9f9396fc05b14398caa60194677c7e13064
Samantha Hahn <smhahn@ucsd.edu>
2016-03-08 14:26:39 -0800

    Update the user cookie email and notification when user settings are saved

--- client/app/scripts/controllers/settings_account.js
+++ client/app/scripts/controllers/settings_account.js
@@ -13,8 +13,29 @@ angular.module('iReceptionistApp')
             $('#page-content').height($rootScope.pageContentHeight());
         });
 
+        toastr.options = {
+            "positionClass": "toast-top-right",
+            "timeOut": "2500"
+        };
+
         $scope.user = $cookies.getObject('user');
-        console.log($scope.user);
+
+        if ($scope.user.settings.receiveEmail === null) {
+            $scope.user.settings.receiveEmail = true;
+        }
+        if ($scope.user.settings.receiveSMS === null) {
+            $scope.user.settings.receiveSMS = true;
+        }
+
+        $scope.updateUser = function() {
+            // User service call
+
+            // Update the user cookie
+            $cookies.putObject('user', $scope.user);
+
+            // Show alert
+            toastr.success("Your settings have been updated!");
+        };
 
         $scope.avatarUpload = DropZone.createNew('#avatarUpload');
         $scope.logoUpload = DropZone.createNew('#logoUpload');
