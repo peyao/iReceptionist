fbb637a6588605654f8e675b67a1e92c5859ba4b
Amanda Bang <arbang@ucsd.edu>
2016-03-09 18:16:41 -0800

    added employee works

--- client/app/scripts/controllers/employees.js
+++ client/app/scripts/controllers/employees.js
@@ -7,14 +7,14 @@
  */
 angular.module('iReceptionistApp')
   .controller('EmployeesCtrl', function($rootScope, $scope, $cookies, UserService) {
-      $rootScope.currentState = 'employees';
-      $('#page-content-ui-view').resize(function () {
-          $('#page-content-ui-view').width($rootScope.pageContentWidth());
-          $('#page-content').height($rootScope.pageContentHeight());
-      });
-      $scope.showEmployeesMore = false;
-      $scope.employees = [];
-      $scope.emp = '';
+    $rootScope.currentState = 'employees';
+    $('#page-content-ui-view').resize(function() {
+      $('#page-content-ui-view').width($rootScope.pageContentWidth());
+      $('#page-content').height($rootScope.pageContentHeight());
+    });
+    $scope.showEmployeesMore = false;
+    $scope.employees = [];
+    $scope.emp = '';
 
     $scope.cancel = function() {
       console.log('resetting form')
@@ -25,84 +25,106 @@ angular.module('iReceptionistApp')
       $scope.newEmp.email = '';
       $scope.inviteForm.$setPristine();
     };
-      var getEmployeeList = function () {
-          UserService.getEmployees(
-              $cookies.get('token'),
-              function (empObj) {
-                  $scope.employees = empObj;
-                  console.log("Grabbing them employees: " + empObj.name);
-              },
-              function (err) {
-                  //$scope.alert.danger = err.errorMsg;
-              }
-          );
-      };
+    var getEmployeeList = function() {
+      UserService.getEmployees(
+        $cookies.get('token'),
+        function(empObj) {
+          $scope.employees = empObj;
+          console.log("Grabbing them employees: " + empObj.name);
+        },
+        function(err) {
+          console.log("employee list error");
+        }
+      );
+    };
 
-      $scope.cancel = function() {
-        console.log('resetting form')
-        $scope.editEmp = {};
-        $scope.newEmp = {};
-        $scope.editEmp.phone = '';
-        $scope.editEmp.name = '';
-        $scope.editEmp.email = '';
-        $scope.editForm.$setPristine();
-        $scope.newEmp.number = '';
-        $scope.newEmp.name = '';
-        $scope.newEmp.email = '';
-        $scope.inviteForm.$setPristine();
-      };
+    $scope.addEmployee = function() {
+      UserService.addEmployee(
+        $cookies.get('token'), {
+          "name": $scope.newEmp.name,
+          "phone": $scope.newEmp.phone,
+          "email": $scope.newEmp.email,
+        },
+        function(empObj) {
+          console.log("Added employee " + empObj.name);
+        },
+        function(err) {
+          console.log("Add employee error");
+        }
+      );
+      $scope.newEmp = {};
+    };
+/*
+    $scope.deleteUser = function () {
+       UserService.deleteEmployee(
+         $cookies.get('token'),
+         "deleteUserId":  ,
+         function (empObj) {
+             console.log("Deleted employee: " + empObj);
+         },
+         function (err) {
+             console.log("Delete employee error");
+         }
+     );
+   };
+*/
+    $scope.cancel = function() {
+      console.log('resetting form')
+      $scope.editEmp = {};
+      $scope.newEmp = {};
+      $scope.editEmp.phone = '';
+      $scope.editEmp.name = '';
+      $scope.editEmp.email = '';
+      $scope.editForm.$setPristine();
+      $scope.newEmp.number = '';
+      $scope.newEmp.name = '';
+      $scope.newEmp.email = '';
+      $scope.inviteForm.$setPristine();
+    };
 
-      getEmployeeList();
-      $scope.inviteEmployee = function () {
-          if ($scope.emp.name && $scope.emp.email && $scope.emp.number) {
-              $scope.employees.push({
-                  "name": $scope.emp.name,
-                  "email": $scope.emp.email,
-                  "phone": $scope.emp.phone
-              });
-          }
-      };
+    getEmployeeList();
 
-      //UserService.addEmployee(
-      //    $cookies.get('token'),
-      //    {
-      //        "name": "required",
-      //        "phone": "required",
-      //        "email": "required",
-      //        "avatar": "optional"
-      //    },
-      //    function (empObj) {
-      //        console.log("Invited employee: " + empObj);
-      //    },
-      //    function (err) {
-      //        console.log("Invite employee error");
-      //    }
-      //);
-      //
-      //UserService.deleteEmployee(
-      //    $cookies.get('token'),
-      //    'userIdToDelete',
-      //    function (empObj) {
-      //        console.log("Deleted employee: " + empObj);
-      //    },
-      //    function (err) {
-      //        console.log("Delete employee error");
-      //    }
-      //);
-      //
-      //UserService.updateUser(
-      //    $cookies.get('token'),
-      //    {
-      //        "field": "value",
-      //        "field": "value"
-      //    },
-      //    function (userObj) {
-      //        console.log("Update employee: " + userObj);
-      //    },
-      //    function (err) {
-      //        console.log("Update employee error");
-      //    }
-      //);
+
+    //UserService.addEmployee(
+    //    $cookies.get('token'),
+    //    {
+    //        "name": "required",
+    //        "phone": "required",
+    //        "email": "required",
+    //        "avatar": "optional"
+    //    },
+    //    function (empObj) {
+    //        console.log("Invited employee: " + empObj);
+    //    },
+    //    function (err) {
+    //        console.log("Invite employee error");
+    //    }
+    //);
+    //
+    //UserService.deleteEmployee(
+    //    $cookies.get('token'),
+    //    'userIdToDelete',
+    //    function (empObj) {
+    //        console.log("Deleted employee: " + empObj);
+    //    },
+    //    function (err) {
+    //        console.log("Delete employee error");
+    //    }
+    //);
+    //
+    //UserService.updateUser(
+    //    $cookies.get('token'),
+    //    {
+    //        "field": "value",
+    //        "field": "value"
+    //    },
+    //    function (userObj) {
+    //        console.log("Update employee: " + userObj);
+    //    },
+    //    function (err) {
+    //        console.log("Update employee error");
+    //    }
+    //);
 
     /*  if(!$cookies.get('employees')){
         UserService.getEmployees(
