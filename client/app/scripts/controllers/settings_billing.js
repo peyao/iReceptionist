8995126a46c918ac9215dff9fa34eb5c193499eb
Samantha Hahn <smhahn@ucsd.edu>
2016-03-10 18:00:17 -0800

    Validate credit card when updating plan

--- client/app/scripts/controllers/settings_billing.js
+++ client/app/scripts/controllers/settings_billing.js
@@ -19,14 +19,58 @@ angular.module('iReceptionistApp')
             "timeOut": "2500"
         };
 
+        $('[data-numeric]').payment('restrictNumeric');
+        $('#credit-card').payment('formatCardNumber');
+        $('#security-code').payment('formatCardCVC');
+        $('#expiration-date').payment('formatCardExpiry');
+
         $scope.user = $cookies.getObject('user');
         $scope.business = $cookies.getObject('business').business;
         $scope.currentPlan = $scope.business.planLevel;
         $scope.planClicked = "";
         $scope.planInfo = "";
 
+        // Credit card info
+        $scope.name = "";
+        $scope.card = "";
+        $scope.securityCode = "";
+        $scope.expirationDate = "";
+
+        $('.credit-card-input').focus(function() {
+           $('.credit-card-error').text("").addClass('hidden');
+        });
+
         $scope.updatePlan = function() {
             // Credit card validation
+            var nameValid = $scope.name !== "";
+            var cardValid = $.payment.validateCardNumber($scope.card);
+            var securityCodeValid = $.payment.validateCardCVC($scope.securityCode);
+            console.log($scope.expirationDate);
+            var date = $scope.expirationDate.split("/");
+            var expirationMonth = date[0].trim();
+            var expirationYear = date[1].trim();
+            var expirationDateValid = $.payment.validateCardExpiry(expirationMonth, expirationYear);
+
+            if (!nameValid) {
+                console.log("name not valid");
+                $('.credit-card-error').text("Please enter the name on your card.").removeClass('hidden');
+                return;
+            }
+            if (!cardValid) {
+                console.log("card not valid");
+                $('.credit-card-error').text("Invalid credit card.").removeClass('hidden');
+                return;
+            }
+            if (!securityCodeValid) {
+                console.log("code not valid");
+                $('.credit-card-error').text("Invalid security code.").removeClass('hidden');
+                return;
+            }
+            if (!expirationDateValid) {
+                console.log("date not valid");
+                $('.credit-card-error').text("Invalid expiration date.").removeClass('hidden');
+                return;
+            }
 
             BusinessService.updateBusiness(
                 $cookies.get('token'),
@@ -47,5 +91,8 @@ angular.module('iReceptionistApp')
                     console.log("Error updating business plan level");
                 }
             );
+
+            // Close the modal
+            $('#plan-modal').modal('hide');
         }
     });
