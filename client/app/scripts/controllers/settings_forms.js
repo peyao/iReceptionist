3f598331cdbeec127cedddf71cba3d4d5b7517e3
Samantha Hahn <smhahn@ucsd.edu>
2016-03-16 16:58:47 -0700

    Employee select in form builder

--- client/app/scripts/controllers/settings_forms.js
+++ client/app/scripts/controllers/settings_forms.js
@@ -9,8 +9,38 @@ angular.module('iReceptionistApp')
     .controller('SettingsFormsCtrl', function($scope, $builder, $validator, $rootScope, $cookies, FormService, DropZone) {
         $rootScope.currentState = 'settings-forms';
 
-        $scope.business = $cookies.getObject('business');
-        console.log($scope.business);
+        $scope.business = $cookies.getObject('business').business;
+        $scope.form = $cookies.getObject('business').form;
+        var employeeSelectObject = {"id":"employee","component":"select","editable":true,"index":3,"label":"Employee","description":"Who are you seeing today?","placeholder":"placeholder","options":["Anyone is fine"],"required":false,"validation":"/.*/","$$hashKey":"object:61"};
+
+        var form = JSON.parse($scope.form);
+        $scope.employeeSelect = false;
+        if (form != null) {
+            for (var i = 0; i < form.length; i++) {
+                if (form[i].hasOwnProperty('id') && form[i]['id'] === 'employee') {
+                    $scope.employeeSelect = true;
+                    break;
+                }
+            }
+        }
+
+        $scope.updateEmployeeSelect = function() {
+            if ($scope.employeeSelect) {
+                // Add employee select to the form
+                $builder.addFormObject('default', employeeSelectObject);
+            }
+            else {
+                // Remove employee select from the form
+                var index;
+                for (var i = 0; i < $builder.forms['default'].length; i++) {
+                    if ($builder.forms['default'][i]['id'] === 'employee') {
+                        index = i;
+                        break
+                    }
+                }
+                $builder.removeFormObject('default', index);
+            }
+        };
 
         /* Add default name field if it hasn't been added already */
         var name;
